a2505122cef58fbaed4ce5e175c16e8289b001d1
mathewthomas <mathewthomas1721@gmail.com>
2017-09-16 23:16:01 -0400

    added ip

--- StoplightShortestPath/client.py
+++ StoplightShortestPath/client.py
@@ -3,7 +3,7 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+host = '192.168.86.27' #socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
