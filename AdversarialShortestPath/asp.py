41097dba6f57847b4290be902867b744179e85c7
Paul Fisher <paulfisher@Pauls-MacBook-Pro.local>
2017-10-09 12:46:30 -0400

    basic algorithms working

--- AdversarialShortestPath/asp.py
+++ AdversarialShortestPath/asp.py
@@ -1,16 +1,38 @@
 import sys
 import numpy as np
 from collections import defaultdict
-from dijkstra import dijkstra
+from dijkstra import *
 
-graph = np.zeros((1000,1000),dtype=int).fill(-1)
+graph = np.zeros((1000,1000))
+graph.fill(-1)
 
-def parsePath(path):
+def parsePath(node, Dpaths):
+	pathList = [node]
+	cost = -1
+	curr = node
+	nxt = node
+	cost, nxt = Dpaths[node]
+	while not (nxt == -1):
+		pathList.append(nxt)
+		node = nxt
+		nxt = Dpaths[node][1]
+	return pathList, cost
+
+def parsePathOld(path):
 	pathList = []
 	while(path):
-		pathList = [path[0]] + pathList
+		pathList.append(path[0])
 		path = path[1]
 	return pathList	
+
+def getCost(path):
+	cost = 0
+	while path[1]:
+		v1 =  path[0]
+		path = path[1]
+		v2 = path[0]
+		cost += graph[v1,v2]
+	return cost
 
 
 def populateGraph(filename):
@@ -20,40 +42,103 @@ def populateGraph(filename):
 	lines = lines[3:]
 
 	g = defaultdict(list)
-        
+    #g_weights = defaultdict(list)
 	for line in lines:
-		graph[int(line[0])][int(line[1])] = 1
-		graph[int(line[1])][int(line[0])] = 1
+		graph[float(line[0]), float(line[1])] = 1
+		graph[float(line[1]), float(line[0])] = 1
 		g[int(line[0])].append((1,int(line[1])))
 		g[int(line[1])].append((1,int(line[0])))
+		#h[int(line[0])].append((1,int(line[1])))
+		#h[int(line[1])].append((1,int(line[0])))
 
-	return start,end,g
+	return start,end,g, #h
+
+def increase_edge(v1, v2, Dpaths):
+	dist1 = Dpaths[v1][0]
+	dist2 = Dpaths[v2][0]
+	dist = min(dist1, dist2)
+	if dist < 0:
+		dist = 0
+	factor = 1.0 + np.sqrt(dist)
+	graph[v1][v2] *= factor
+	graph[v2][v1] *= factor
+	return factor
+
+
+def get_move(Dpaths, graphMat, start, end):
+	path, dist = parsePath(start, Dpaths)
+	cost = float('inf')
+	for p in AllPathsN(graphMat, end, start, dist, Dpaths):
+		c = getCost(p)
+		if (c < cost):
+			path = parsePathOld(p)
+			cost = c
+	return path[1]
+
+def get_A_move(Dpaths, graphMat, start, end):
+	path, distance = parsePath(start, Dpaths)
+	cost = float('inf')
+	for p in AllPathsN(graphMat, end, start, distance, Dpaths):
+		c = getCost(p)
+		if (c < cost):
+			path = parsePathOld(p)
+			cost = c
+	print path
+	max_penalty = -1
+	move = (-1,-1)
+	for i in range(1,len(path)):
+		v1 = path[i - 1]
+		v2 = path[i]
+		dist1 = Dpaths[v1][0]
+		dist2 = Dpaths[v2][0]
+		dist = min(dist1, dist2)
+		if (dist < 0 ):
+			dist = 0
+		factor = 1.0 + np.sqrt(dist)
+		penalty = graph[v1][v2] * factor ** (distance - dist) - graph[v1][v2]
+		if penalty > max_penalty:
+			max_penalty = penalty
+			move = (v1,v2)
+	return move
+
+
+def game(filename):
+	start,end,graphMat = populateGraph(filename)
+	Dpaths = dijkstra(graphMat,end)
+	current_v = start
+	total_cost = 0
+	for p in AllPathsN(graphMat, end, start, Dpaths[start][0], Dpaths):
+		print parsePathOld(p)
+	move = get_move(Dpaths, graphMat, start, end)
+	total_cost += graph[current_v][move]
+	print "Player moves from vertex " + str(current_v) + " to vertex " + str(move) + " and pays "  + str(graph[current_v][move]) + ". Total cost: " + str(total_cost) + "\n"
+	current_v = move
+	while (current_v != end):
+		a_move = get_A_move(Dpaths, graphMat, current_v, end)
+		fac = increase_edge(a_move[0], a_move[1], Dpaths)
+		print "Adversary increases edge " + str(a_move) + " by a factor of " + str(fac) + ". New Cost: " + str(graph[a_move[0], a_move[1]]) + "\n"
+		move = get_move(Dpaths, graphMat, current_v, end)
+		total_cost += graph[current_v][move]
+		print "Player moves from vertex " + str(current_v) + " to vertex " + str(move) + " and pays "  + str(graph[current_v][move]) + ". Total cost: " + str(total_cost) + "\n"
+		current_v = move
+
+
+
+
 
 filename = sys.argv[1]
-start,end,graphMat = populateGraph(filename)
+game(filename)
+"""start,end,graphMat = populateGraph(filename)
+
 
 dijkstraDistMat = np.zeros((1000,1000),dtype = [('cost','i4'),('nxt','i4')])
 dijkstraDistMat['cost'] = -1
 dijkstraDistMat['nxt'] = -1
-cost,path = dijkstra(graphMat,key1,key2)
-path = parsePath(path)
+Dpaths = dijkstra(graphMat,end)
+path, cost = parsePath(start, Dpaths)
+print path, cost
+for p in AllPathsN(graphMat, end, start, cost, Dpaths):
+	print str(parsePathOld(p)) + " cost: " + str(getCost(p))  + "\n"
 
-#ignore the stuff below this for now
+	"""
 
-#print dijkstraDistMat
-'''for key1 in graphMat:
-	for key2 in graphMat:
-		if key1 != key2:
-			if dijkstraDistMat[key1][key2]['nxt'] == -1:
-				cost,path = dijkstra(graphMat,key1,key2)
-				#print len(path)
-				path = parsePath(path)
-				pathLen = len(path)
-				for i in range(pathLen) : 
-					idx = pathLen-i
-					dijkstraDistMat[key1][idx] = (cost-edge[,path[i]) #fix
-					dijkstraDistMat[idx][key1] = (cost,path[len(path)-2])'''
-#print dijkstraDistMat[start][end]			
-#cost,path = dijkstra(graphMat,start,end)
-#print parsePath(path)
-#print dijkstraDistMat[508]
--- AdversarialShortestPath/dijkstra.py
+++ AdversarialShortestPath/dijkstra.py
@@ -1,21 +1,45 @@
 from collections import defaultdict
 from heapq import *
+import numpy as np
 
-def dijkstra(g, f, t):
-	
+def dijkstra(g, f):
+	dijkstraPathMap = np.zeros(1000,dtype = [('cost','i4'),('nxt','i4')])	
+	dijkstraPathMap.fill(-1)
 	q, seen = [(0,f,())], set()
 	while q:
 		(cost,v1,path) = heappop(q)
 		if v1 not in seen:
 			seen.add(v1)
+			if (not path == ()):
+				dijkstraPathMap[v1] = (cost, path[0])
 			path = (v1, path)
-			
+			"""
 			if v1 == t: 
 				return cost, path
-				
+				"""
 			for c, v2 in g.get(v1, ()):
 				if v2 not in seen:
 					heappush(q, (cost+c, v2, path))
 
-	return float("inf")
+	return dijkstraPathMap
 
+
+def AllPathsN(g, f, t, N, dists):
+	paths = []
+	q, seen = [(0,f,())], set()
+	while q:
+		(cost,v1,path) = heappop(q)
+		if (v1 not in seen) or cost == dists[v1][0]:
+			seen.add(v1)
+			
+			path = (v1, path)
+			
+			if v1 == t: 
+				paths.append(path)
+				
+			for c, v2 in g.get(v1, ()):
+				newC = cost + c
+				if (v2 not in seen or newC == dists[v2][0]) and newC <= N :
+					heappush(q, (newC, v2, path))
+
+	return paths
