7ae6ba243ed13d2333ea762dbda6c33bc9b120e0
mathewthomas <mathewthomas1721@gmail.com>
2017-10-29 23:21:43 -0400

    pathCost function first draft

--- DancingWithoutStars/moving.py
+++ DancingWithoutStars/moving.py
@@ -5,5 +5,62 @@ def checkFinished(curr,final):
     return True
 
 def pathCost(initialPos, finalPos, stars):
+    cost = 0
+    skip = []
     while not checkFinished(initialPos,finalPos):
+        print skip
         for i in range(len(initialPos)):
+            print initialPos[i]
+            if initialPos[i] != finalPos[i]:
+                if initialPos[i][0] != finalPos[i][0] :
+                    if i in skip:
+                        skip.remove(i)
+                    else:
+                        if initialPos[i][0] < finalPos[i][0]:
+                            test = initialPos[i]
+                            test[0] = test[0] + 1
+                            if test in stars:
+
+                                if test[1]<=finalPos[i][1]:
+                                    test[1] = test[1] + 1
+                                else:
+                                    test[1] = test[1] - 1
+
+                                test[0] = test[0] - 1
+
+                            initialPos[i] = test
+                        else :
+                            test = initialPos[i]
+                            test[0] = test[0] - 1
+                            if test in stars:
+
+                                if test[1]<=finalPos[i][1]:
+                                    test[1] = test[1] + 1
+                                else:
+                                    test[1] = test[1] - 1
+
+                                test[0] = test[0] + 1
+                            initialPos[i] = test
+                else:
+                    if initialPos[i][1] < finalPos[i][1]:
+                        test = initialPos[i]
+                        test[1] = test[1] + 1
+                        if test in stars:
+                            test[0] = test[0] + 1
+                            #test[1] = test[1] - 1
+                            skip.append(i)
+                        initialPos[i] = test
+                    else :
+                        test = initialPos[i]
+                        test[1] = test[1] - 1
+                        if test in stars:
+                            test[0] = test[0] + 1
+                            #test[1] = test[1] + 1
+                            skip.append(i)
+                        initialPos[i] = test
+        cost = cost + 1
+
+    return cost
+a = [[0,0]]
+b = [[-5,7]]
+print pathCost(a,b,[[-5,4]])
