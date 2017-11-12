96bcff071275eb179fb268012a07df5eb0b5d082
mathewthomas <mathewthomas1721@gmail.com>
2017-11-12 01:46:57 -0500

    Base Strategy Commit

--- Auction/BabyClient.py
+++ Auction/BabyClient.py
@@ -5,6 +5,17 @@ from client import Client
 import time
 import random
 
+def rankArtist(auctions, numArtists, req):
+    artistRanks = [0] * numArtists
+    countArtist = [0] * numArtists
+    for i in range(len(auctions)):
+        curr_artist = int(auctions[i][1:])
+        countArtist[curr_artist] += 1
+
+        if countArtist[curr_artist] == req[curr_artist]:
+            artistRanks[curr_artist] = i
+    return artistRanks
+
 def check_game_status(state):
 
     if state['bid_winner'] is not None:
@@ -19,10 +30,15 @@ def check_game_status(state):
         print('Game over\n{}\n'.format(state['reason']))
         exit(0)
 
-def calculate_bid(game_state):
-
-    """Insert algorithm here"""
-    return 1
+def calculate_bid(ranks,item,curr_wealth):
+    rank1 = min(ranks)
+    if item == ranks.index(rank1):
+        if rank1>0:
+            return int(curr_wealth/rank1)
+        else :
+            return curr_wealth
+    else:
+        return 0
 
 if __name__ == '__main__':
 
@@ -36,15 +52,34 @@ if __name__ == '__main__':
     required_count = client.required_count
     auction_items = client.auction_items
     init_wealth = client.init_wealth
-
+    player_count = client.player_count
+    curr_wealth = init_wealth
+    rem_auctions = auction_items
+    req = [required_count] * artists_types
+    '''print ("RANKS")
+    print(rankArtist(auction_items, artists_types, required_count))'''
+    '''print ("artists_types")
+    print (artists_types)
+    print ("required_count")
+    print (required_count)
+    print ("auction_items")
+    print (auction_items)
+    print ("init_wealth")
+    print (init_wealth)'''
     current_round = 0
 
     while True:
-        bid_amt = calculate_bid(None)
+
+        ranks = rankArtist(rem_auctions, artists_types, req)
+        bid_amt = calculate_bid(ranks, int(rem_auctions[0][1:]), curr_wealth)
         client.make_bid(auction_items[current_round], bid_amt)
 
         # after sending bid, wait for other player
         game_state = client.receive_round()
+        if game_state['bid_winner'] == 'BabySnakes':
+            curr_wealth = curr_wealth - game_state['winning_bid']
+            req[int(game_state['bid_item'][1:])] -= 1
+        rem_auctions = rem_auctions[1:]
         check_game_status(game_state)
 
         current_round += 1
