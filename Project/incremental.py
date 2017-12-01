28f86b495cd2f5ab77061c860c0c22003dbc964e
Paul Fisher <paulfisher@Pauls-MacBook-Pro.local>
2017-12-01 11:26:43 -0500

    added penalty count +tool tip text for adversary

--- Project/incremental.py
+++ Project/incremental.py
@@ -1,4 +1,11 @@
 
+
+def toForm(num):
+    if (num > 1000000):
+        return num.toExponential(3)
+    else:
+        return num.toFixed(0)
+
 class Property:
     def __init__(self,name, base_cost, cost_mult, base_income):
         self.name = name
@@ -68,7 +75,7 @@ class Game:
             self.properties.append(Property(prop[0], prop[1], prop[2], prop[3]))
         for ug in upgrades:
             self.properties[ug[1]].upgrades_Available.append(Upgrade(ug[0], ug[2], ug[3]))
-
+        
 
     def buy_prop(self, prop_to_buy):
         prop = self.properties[prop_to_buy]
@@ -138,7 +145,7 @@ class Two_Player_Game(Game):
 
 class incremental:
 
-    properties = [list (tupl) for tupl in [
+    properties = [list (tupl) for tupl in [ 
         ('Burger Stand', 10.0, 1.15, 1.0),
         ('Diner', 100.0, 1.15, 8.0),
         ('Gas Station', 1000.0, 1.15, 60.0),
@@ -146,10 +153,10 @@ class incremental:
         ('Bank',  65000.0, 1.15, 2500.0),
         ('Department Store',  210000.0, 1.15, 9000.0),
         ('Auto Manufacturer',  4000000.0, 1.15, 100000.0),
-        ('Conglomerate',  100000000.0, 1.15, 2000000.0)
+        ('Multinational',  100000000.0, 1.15, 2000000.0) 
     ]]
 
-    upgrades = [list (tupl) for tupl in [
+    upgrades = [list (tupl) for tupl in [ 
         ('Better Burgers', 0, 10, 2.0),
         ('Saltier Fries', 1, 10, 2.0),
         ('Premium Gas', 2, 10, 2.0),
@@ -157,23 +164,23 @@ class incremental:
         ('Subprime Mortgage', 4, 10, 2.0),
         ('New Advertising Campaign', 5, 10, 2.0),
         ('Cheat Safety Regulations', 6, 10, 2.0),
-        ('Outsourcing', 7, 10, 2.0),
+        ('Outsourcing', 7, 10, 2.0),     
     ]]
 
-    penalties = [list (tupl) for tupl in [
-        ('Cost Increase 1', 0, 0, 1.15),
-        ('Cost Increase 2', 0, 1, 1.15),
-        ('Cost Increase 3', 0, 2, 1.15),
-        ('Cost Increase 4', 0, 3, 1.15),
-        ('Cost Increase 5', 0, 4, 1.15),
-        ('Cost Increase 6', 0, 5, 1.15),
-        ('Cost Increase 7', 0, 6, 1.15),
-        ('Cost Increase 8', 0, 7, 1.15),
+    penalties = [list (tupl) for tupl in [ 
+        ('Cost Increase 1', 0, 0, 1.25),
+        ('Cost Increase 2', 0, 1, 1.25),
+        ('Cost Increase 3', 0, 2, 1.25),
+        ('Cost Increase 4', 0, 3, 1.25),
+        ('Cost Increase 5', 0, 4, 1.25),
+        ('Cost Increase 6', 0, 5, 1.25),
+        ('Cost Increase 7', 0, 6, 1.25),
+        ('Cost Increase 8', 0, 7, 1.25),
         ('Penalty 1', 1, 0.5, 100),
-
+  
     ]]
-
-
+        
+        
     def __init__ (self, two_player):
         window.setInterval(self.Update, 1000)
         self.two_player = two_player
@@ -181,69 +188,49 @@ class incremental:
             self.gm = Two_Player_Game(self.properties,self.upgrades, self.penalties)
         else:
             self.gm = Game(self.properties, self.upgrades)
-
-
+        
+        
     def BuyProp(self, n):
         self.gm.buy_prop(n - 1)
         prop = self.gm.properties[n-1]
-        if (prop.cost > 1000000):
-            pcost = prop.cost.toExponential(3)
-        else:
-            pcost = prop.cost.toFixed(0)
-        document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, pcost)
-        if (self.gm.currency < 1000000):
-            curr = self.gm.currency.toFixed(0)
-        else:
-            curr = self.gm.currency.toExponential(3)
-        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(curr)
-        document.getElementById('tt' + str(n)).innerHTML = "Your {}s are producing ${} per second.".format(prop.name, prop.total_income)
-
+        document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, toForm(prop.cost))
+        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(toForm(self.gm.currency))
+        document.getElementById('tt' + str(n)).innerHTML = "Your {}s are producing ${} per second.".format(prop.name, toForm(prop.total_income))
+            
 
     def UpgradeProp(self, n):
         self.gm.upgrade_prop(n - 1)
-        if (self.gm.currency < 1000000):
-            curr = self.gm.currency.toFixed(0)
-        else:
-            curr = self.gm.currency.toExponential(3)
-        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(curr)
+        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(toForm(self.gm.currency))
         ug = prop.get_next_upgrade()
-        document.getElementById('ttu' + str(n)).innerHTML = 'Purchase "{}" for ${}. Multipy all {} income by {}'.format(ug.name, ug.cost, prop.name, ug.mult)
+        document.getElementById('ttu' + str(n)).innerHTML = 'Purchase "{}" for ${}. Multipy all {} income by {}'.format(ug.name, toForm(ug.cost), prop.name, ug.mult)
 
 
     def ApplyPenalty(self, n):
         self.gm.applyPenalty(n - 1)
-        document.getElementById ('adv1') .innerHTML = 'Adversary has {} penalties to apply. Next Penalty {}.'.format(self.gm.pen_count, self.gm.penalties[0].name)
+        document.getElementById ('advcount') .innerHTML = 'Adversary has {} penalties to apply.'.format(self.gm.pen_count)
         for n in [1,2,3,4,5,6,7,8]:
             prop = self.gm.properties[n-1]
-            if (prop.cost > 1000000):
-                pcost = prop.cost.toExponential(3)
-            else:
-                pcost = prop.cost.toFixed(0)
-            document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, pcost)
+            document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, toForm(prop.cost))
 
 
     def Update (self):
         self.gm.cycle()
-        if (self.gm.currency < 1000000):
-            curr = self.gm.currency.toFixed(0)
-        else:
-            curr = self.gm.currency.toExponential(3)
-        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(curr)
+        document.getElementById('cash').innerHTML = 'Total Cash: ${}'.format(toForm(self.gm.currency))
         if self.two_player:
-            document.getElementById ('adv1') .innerHTML = 'Adversary has {} penalties to apply. Next Penalty {}.'.format(self.gm.pen_count, self.gm.penalties[0].name)
+            document.getElementById ('advcount') .innerHTML = 'Adversary has {} penalties to apply.'.format(self.gm.pen_count)
+            for n in [1,2,3,4,5,6,7,8]:
+                prop = self.gm.properties[n-1]
+                document.getElementById ('tta'+str(n)) .innerHTML = 'Increase cost of {} by a factor of {}'.format(prop.name, self.gm.penalties[n].mult.toFixed(2))
         for n in [1,2,3,4,5,6,7,8]:
             prop = self.gm.properties[n-1]
-            if (prop.cost > 1000000):
-                pcost = prop.cost.toExponential(3)
-            else:
-                pcost = prop.cost.toFixed(0)
-            document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, pcost)
-            document.getElementById('tt' + str(n)).innerHTML = "Your {}s are producing ${} per second.".format(prop.name, prop.total_income)
+            document.getElementById ('prop'+ str(n)) .innerHTML = 'You own {} {}s. Cost for next: ${}'.format (prop.count, prop.name, toForm(prop.cost))
+            document.getElementById('tt' + str(n)).innerHTML = "Your {}s are producing ${} per second.".format(prop.name, toForm(prop.total_income))
             ug = prop.get_next_upgrade()
-            document.getElementById('ttu' + str(n)).innerHTML = 'Purchase "{}" for ${}. Multipy all {} income by {}'.format(ug.name, ug.cost, prop.name, ug.mult)
+            document.getElementById('ttu' + str(n)).innerHTML = 'Purchase "{}" for ${}. Multipy all {} income by {}'.format(ug.name, toForm(ug.cost), prop.name, ug.mult)
 
 
 
 
-
+            
 game = incremental (True)
+    
